da3e75b8dd9b1f8bf7258a14be2ca8e42487c9d3
Tom Leb <tomleb3@gmail.com>
2021-04-15 15:16:58 +0300

    about-page

--- dist/js/about.73c46966.js
+++
@@ -1,6 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{a1d1:function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("section",{staticClass:"about main-layout m-page"},[a("h1",[t._v("This is an about page")])])}],u={},c=u,i=a("2877"),l=Object(i["a"])(c,n,s,!1,null,null,null);e["default"]=l.exports}}]);
-<<<<<<< HEAD:dist/js/about.4116acde.js
-//# sourceMappingURL=about.4116acde.js.map
-=======
-//# sourceMappingURL=about.73c46966.js.map
->>>>>>> d7279ff1dbf936c3b52aa781e13d0e57215406e1:dist/js/about.73c46966.js
